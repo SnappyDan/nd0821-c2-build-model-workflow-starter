ac3310404d2baf41249684cd481e81e3e1c28157
Dan <spinydaniel@gmail.com>
2022-03-19 17:24:40 +0300

    Correct basic_cleaning to drop rows in the dataset that are not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -43,6 +43,10 @@ def go(args):
     # Drop null values
     #df = df.dropna().reset_index(drop=True)
 
+    # drop rows in the dataset that are not in the proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
 
